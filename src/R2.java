5e51554693df62fb4179e1ad7f7aa4b4fa67582e
semtlove <semtlove@gmail.com>
2016-08-08 02:07:15 +0900

    과거1등번호4개이상똑같은경우체크()

--- src/R2.java
+++ src/R2.java
@@ -1,11 +1,23 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 
 public class R2 {
 
 //	String str제외수 = "";
 	String str제외수 = "04,05,19,07,12,42,6,32,45";
 	int i로또게임갯수 = 20;
+	public static String[] dmOne = new String[10]; // 년 ,회차 ,추첨일 ,1 ,2 ,3 ,4 ,5 ,6 ,뽀너스
+	public static List arrOne = new ArrayList();
 
 	public static void main(String[] args) {
 
@@ -17,6 +29,8 @@ public class R2 {
 
 		int[] lottoNumbers = new int[6];
 		Random rnd = new Random();
+		
+		과거1등번호메모리load();
 
 		while (i로또게임갯수 > 0) {
 
@@ -39,8 +53,15 @@ public class R2 {
 			rotto = rotto.replaceAll("\\[", "");
 			rotto = rotto.replaceAll("\\]", "");
 			String[] dmRotto = rotto.split(",");
+			
+			for (int inx = 0; inx < dmRotto.length; inx++) {
+				dmRotto[inx] = dmRotto[inx].trim();
+				dmRotto[inx] = 한자리인경우앞에0을붙임(dmRotto[inx]);
+			}
 
 			boolean ok = true;
+			ok = 과거1등번호4개이상똑같은경우체크(dmRotto);
+			if (ok == false) continue;
 			ok = 뒷숫자가같은지체크(dmRotto);
 			if (ok == false) continue;
 			ok = 제외수포함체크(dmRotto);
@@ -52,13 +73,160 @@ public class R2 {
 
 		}
 	}
+	
+	public void 과거1등번호메모리load() {
+		
+		
+		HSSFRow row;
+		HSSFCell cell;
+
+		try {
+			String rootPath = System.getProperty("user.dir");
+			String strExcelPath = rootPath + File.separator + "DATA" + File.separator + "Rotto2003.xls";
+			FileInputStream inputStream = new FileInputStream(strExcelPath);
+			POIFSFileSystem fileSystem = new POIFSFileSystem(inputStream);
+			HSSFWorkbook workbook = new HSSFWorkbook(fileSystem);
+			//sheet수 취득
+			int sheetCn = workbook.getNumberOfSheets();
+			System.out.println("sheet수 : " + sheetCn);
+			
+			for(int cn = 0; cn < sheetCn; cn++){
+				System.out.println("취득하는 sheet 이름 : " + workbook.getSheetName(cn));
+				System.out.println(workbook.getSheetName(cn) + " sheet 데이터 취득 시작");
+				
+				//cn번째 sheet 정보 취득
+				HSSFSheet sheet = workbook.getSheetAt(cn);
+				
+				//취득된 sheet에서 rows수 취득
+				int rows = sheet.getPhysicalNumberOfRows();
+				System.out.println(workbook.getSheetName(cn) + " sheet의 row수 : " + rows);
+				
+				//취득된 row에서 취득대상 cell수 취득
+				int cells = sheet.getRow(cn).getPhysicalNumberOfCells(); //
+				System.out.println(workbook.getSheetName(cn) + " sheet의 row에 취득대상 cell수 : " + cells);
+				
+				System.out.println("------------------------------------------------------------------------------");
+				
+				int iCellPos = 1;
+				
+				for (int r = 0; r < rows; r++) {
+					
+					if ( r < 3 ) {
+						continue;
+					}
+					
+					String value = null;
+					iCellPos = 1;
+					dmOne = new String[10];
+					
+					row = sheet.getRow(r);
+					if (row != null) {
+						for (int c = 0; c < cells; c++) {
+							
+							if ( ( c == 1 || c == 2 || c >=13 ) == false ) {
+								continue;
+							}
+							
+							cell = row.getCell(c);
+							if (cell != null) {
+								
+								switch (cell.getCellType()) {
+								case HSSFCell.CELL_TYPE_FORMULA:
+									value = cell.getCellFormula();
+									break;
+								case HSSFCell.CELL_TYPE_NUMERIC:
+									value = "" + cell.getNumericCellValue();
+									break;
+								case HSSFCell.CELL_TYPE_STRING:
+									value = "" + cell.getStringCellValue();
+									break;
+								case HSSFCell.CELL_TYPE_BLANK:
+									value = "" + cell.getBooleanCellValue();
+									break;
+								case HSSFCell.CELL_TYPE_ERROR:
+									value = "" + cell.getErrorCellValue();
+									break;
+								default:
+									value = "[type?]";
+									break;
+								}
+								System.out.print(value + "\t");
+							} else {
+								System.out.print("[null]\t");
+							} // if
+							
+							if ( c >=13 ) {
+								float f = Float.parseFloat(value);
+								int i = (int)f;
+								value = String.valueOf(i);
+							}
+							
+							value = R2.한자리인경우앞에0을붙임(value);
+							dmOne[iCellPos++] = value;
+							
+						} // for(c)
+						
+						arrOne.add(dmOne);
+						
+						System.out.print("\n");
+					} // if
+				} // for(r)
+				System.out.println(workbook.getSheetName(cn) + " sheet 데이터 취득 종료");
+			}// for(cn)
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	public boolean 과거1등번호4개이상똑같은경우체크(String[] dmRotto) {
+		
+		boolean bRtnValue = true;
+		
+		String str과거1등번호 = "";
+		String str현재추출번호 = "";
+		String strTmp과거1등 = "";
+		String strTmp현재추출 = "";
+		int i같은갯수 = 0;
+		for (int inx=0; inx < arrOne.size(); inx++ ) {
+			String[] dm과거1등번호 = (String[])arrOne.get(inx);
+			i같은갯수 = 0;
+			
+			for (int idx=3; idx < 9; idx++) {
+				strTmp과거1등 = dm과거1등번호[idx];
+				
+				for (int idxx=0; idxx < 6; idxx++) {
+					strTmp현재추출 = dmRotto[idxx];
+					
+					if ( strTmp과거1등.equals(strTmp현재추출) == true ) {
+						++i같은갯수;
+					}
+				}
+				
+			}
+			
+			
+			if ( i같은갯수 >= 4 ) {
+				bRtnValue = false;
+				break;
+			}
+		}
+		
+		return bRtnValue;
+	}
+	
+	public boolean 전회차에번호가1개or2개포함인치체크() {
+		
+		return false;
+	}
+	
 
 	public boolean 제외수포함체크(String[] dmRotto) {
 
 		boolean rValue = true;
 
 		for (int inx = 0; inx < dmRotto.length; inx++) {
-			dmRotto[inx] = 한자리인경우앞에0을붙임(dmRotto[inx]);
 
 			if (str제외수.indexOf(dmRotto[inx]) > -1) {
 				rValue = false;
@@ -74,7 +242,6 @@ public class R2 {
 		String str뒷자리모음 = "";
 		int i같은수 = 0;
 		for (int inx = 0; inx < dmRotto.length; inx++) {
-			dmRotto[inx] = 한자리인경우앞에0을붙임(dmRotto[inx]);
 			String 한자리 = dmRotto[inx].substring(1, 2);
 
 			if (str뒷자리모음.indexOf(한자리) > -1) {
@@ -89,14 +256,17 @@ public class R2 {
 
 		boolean rValue = false;
 
-		if (i같은수 > 0) {
+		if (i같은수 >= 3) {
+			rValue = false;
+		}
+		else if (i같은수 <= 2 ) {
 			rValue = true;
 		}
 
 		return rValue;
 	}
 
-	public String 한자리인경우앞에0을붙임(String s숫자) {
+	public static String 한자리인경우앞에0을붙임(String s숫자) {
 		if (s숫자.length() <= 1) {
 			s숫자 = "0" + s숫자;
 		}
